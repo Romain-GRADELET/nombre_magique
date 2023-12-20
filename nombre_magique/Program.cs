a3489a12dece96ef265ded301f8681d06bd79132
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-12-20 17:15:16 +0100

    Add life for user and comment

--- nombre_magique/Program.cs
+++ nombre_magique/Program.cs
@@ -37,10 +37,57 @@ namespace nombre_magique
             const int NOMBRE_MIN = 1;
             const int NOMBRE_MAX = 10;
 
+            Random rand = new Random();
+            int NOMBRE_MAGIQUE = rand.Next(NOMBRE_MIN, NOMBRE_MAX +1);
 
-            int nombreTest = DemanderNombre(NOMBRE_MIN, NOMBRE_MAX);
-            Console.WriteLine("Votre nombre est: " + nombreTest);
+            int nombre = NOMBRE_MAGIQUE + 1;
+
+            int nbVies = 4;
+
+
+            while (nbVies > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Vies restantes : " + nbVies);
+                nombre = DemanderNombre(NOMBRE_MIN, NOMBRE_MAX);
+
+                if (nombre > NOMBRE_MAGIQUE)
+                {
+                    Console.WriteLine("Le nombre magique est plus petit");
+                }
+                else if (nombre < NOMBRE_MAGIQUE)
+                {
+                    Console.WriteLine("Le nombre magique est plus grand");
+                }
+                else
+                {
+                    // Pour intérrompre l'itération
+                    break;
+                }
+                nbVies--;
+            }
+
+            if (nombre == NOMBRE_MAGIQUE)
+            {
+                Console.WriteLine($"Bravo, vous avez trouvé le nombre magique! Il vous restait {nbVies} vie");
+            }
+            else
+            {
+                Console.WriteLine("Vous avez perdu, le nombre magique était: " + NOMBRE_MAGIQUE);
+            }
+
+
+/*
+            Random rand = new Random();
+
+            for (int i = 0; i < 11;  i++)
+            {
+                int valeurAléatoire = rand.Next(1, 11);
+                Console.WriteLine(valeurAléatoire);
+            }
+*/
 
         }
     }
+
 }
